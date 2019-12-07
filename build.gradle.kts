9384ab6c2d7f81d45c4a0198e67d9839626a515c
Owen Leather <owenleather22@mittymonarch.com>
2019-12-06 20:38:12 -0800

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,5 @@
-
-import edu.wpi.first.gradlerio.GradleRIOPlugin
-import edu.wpi.first.gradlerio.frc.FRCJavaArtifact
-import edu.wpi.first.gradlerio.frc.FRCPlugin
-import edu.wpi.first.gradlerio.frc.RoboRIO
-import edu.wpi.first.gradlerio.wpi.dependencies.WPIVendorDepsExtension
 import edu.wpi.first.toolchain.NativePlatforms
-import jaci.gradle.deploy.artifact.ArtifactsExtension
-import jaci.gradle.deploy.artifact.FileTreeArtifact
-import jaci.gradle.deploy.target.TargetsExtension
+
 plugins {
     id("java")
     id("edu.wpi.first.GradleRIO")  version "2019.4.1"
@@ -16,11 +8,11 @@ plugins {
 group = "com.github.MittyRobotics"
 version = "1.0-SNAPSHOT"
 
-
 repositories {
     jcenter()
     maven(url = "https://jitpack.io")
 }
+
 
 dependencies {
     // WPILib
@@ -32,3 +24,4 @@ dependencies {
     // https://mvnrepository.com/artifact/org.jfree/jfreechart
     compile(group = "org.jfree", name = "jfreechart", version = "1.5.0")
 }
+
