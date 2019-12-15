ec28aa3dfae54e3bbd532cb7fc2a8023ac8f4d30
Owen Leather <owenleather22@mittymonarch.com>
2019-12-14 23:28:41 -0800

    Update SimMain.java

--- simulation/src/main/java/com/github/mittyrobotics/simulation/SimMain.java
+++ simulation/src/main/java/com/github/mittyrobotics/simulation/SimMain.java
@@ -1,9 +1,14 @@
 package com.github.mittyrobotics.simulation;
 
+import com.github.mittyrobotics.simulation.sim.RobotSimManager;
+import com.github.mittyrobotics.simulation.util.SimOI;
+import com.github.mittyrobotics.simulation.util.SimSampleDrivetrain;
+import com.github.mittyrobotics.simulation.util.SimSampleRobot;
+
 public class SimMain {
     public static void main(String[] args) {
         SimSampleRobot robot = new SimSampleRobot();
-        RobotSimManager.getInstance().setupRobotSimManager(robot,SimSampleDrivetrain.getInstance(), 125,7,2,20,30,0.02);
+        RobotSimManager.getInstance().setupRobotSimManager(robot, SimSampleDrivetrain.getInstance(), 125,7,2,20,30,0.02);
         SimOI.getInstance();
     }
 
