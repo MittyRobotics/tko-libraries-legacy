b984e97de6c31b0028f878821d1bab2470811696
Owen Leather <owenleather22@mittymonarch.com>
2019-12-15 23:07:46 -0800

    working on closest point calculations for Path

--- path-generation/src/main/java/com/github/mittyrobotics/path/generation/Main.java
+++ path-generation/src/main/java/com/github/mittyrobotics/path/generation/Main.java
@@ -1,5 +1,6 @@
 package com.github.mittyrobotics.path.generation;
 
+import com.github.mittyrobotics.datatypes.positioning.Position;
 import com.github.mittyrobotics.datatypes.positioning.Transform;
 import com.github.mittyrobotics.path.generation.paths.CubicHermitePath;
 import com.github.mittyrobotics.path.generation.splines.CubicHermiteSpline;
@@ -14,13 +15,29 @@ public class Main {
 		
 		Graph graph = new Graph();
 		
+		graph.resizeGraph(-10,110,-10,110);
+		
 		graph.getChart().removeLegend();
 		CubicHermitePath path = new CubicHermitePath(new Transform[]{
 				new Transform(0, 0, 0),
-				new Transform(100, 24, 0),
-				new Transform(200, 24, 0)
+				new Transform(100, 24, 0)
 		});
-		
-		graph.addDataset(GraphManager.getInstance().graphParametric(path.getParametrics(), 2, 1, "spline", Color.cyan));
+		Transform transform = new Transform(0,0);
+		while(true){
+			graph.clearGraph();
+			graph.addDataset(GraphManager.getInstance().graphParametric(path.getParametrics(), 2, 1, "spline", Color.cyan));
+			Position closestPos = path.getClosestPoint(transform.getPosition(),0,false,3);
+			graph.addDataset(GraphManager.getInstance().graphArrow(new Transform(closestPos,90),5,2,"asdf",Color.green));
+			
+			transform.setPosition(transform.getPosition().add(new Position(.5,0)));
+			
+			graph.addDataset(GraphManager.getInstance().graphArrow(transform,5,2,"asdf",Color.white));
+			
+			try {
+				Thread.sleep(30);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 }
--- path-generation/src/main/java/com/github/mittyrobotics/path/generation/paths/Path.java
+++ path-generation/src/main/java/com/github/mittyrobotics/path/generation/paths/Path.java
@@ -1,6 +1,7 @@
 package com.github.mittyrobotics.path.generation.paths;
 
 import com.github.mittyrobotics.datatypes.path.Parametric;
+import com.github.mittyrobotics.datatypes.positioning.Position;
 import com.github.mittyrobotics.datatypes.positioning.Transform;
 
 public abstract class Path {
@@ -13,6 +14,21 @@ public abstract class Path {
 	}
 	
 	public abstract void generateParametricEquations();
+	
+	public Position getPosition(double t) {
+		//Convert t from 0 to 1 to 0 to waypoints.length-1 so that the t value represents all parametric equation
+		//segments of the total path.
+		t = t * parametrics.length;
+		
+		for (int i = 0; i < parametrics.length; i++) {
+			//Find which parametric equation segment that t falls in
+			if (t >= i && t <= i + 1) {
+				//return the Transform from the segment that it falls in
+				return parametrics[i].getPosition(t - i);
+			}
+		}
+		return new Position();
+	}
 	
 	public Transform getTransform(double t) {
 		//Convert t from 0 to 1 to 0 to waypoints.length-1 so that the t value represents all parametric equation
@@ -27,6 +43,43 @@ public abstract class Path {
 			}
 		}
 		return new Transform();
+	}
+	
+	public Position getClosestPoint(Position referencePosition, double distanceShift, boolean reversed, double steps){
+		double minT = 0;
+		double maxT = 0;
+		double closestDistance = 9999;
+		for(int i = 1; i < steps+1; i++){
+			double t = i/steps;
+			double distanceClosest = getPosition(t).distance(referencePosition);
+			double distanceBehind = getPosition((i-1)/steps).distance(referencePosition);
+			double distanceAhead = getPosition((i+1)/steps).distance(referencePosition);
+			if(distanceBehind + distanceClosest > distanceAhead+distanceClosest){
+				if(distanceBehind < closestDistance){
+					closestDistance = distanceBehind;
+					minT = (i)/steps;
+				}
+			}
+			else{
+				if(distanceClosest < closestDistance){
+					closestDistance = distanceClosest;
+					minT = (i-1)/steps;
+					maxT = i/steps;
+				}
+			}
+		}
+		
+		double secondSteps = 1000;
+		closestDistance = 9999;
+		for(double t = minT; t < maxT; t += 0.001){
+			double distance = getPosition(t).distance(referencePosition);
+			if(distance < closestDistance){
+				closestDistance = distance;
+				minT = t;
+			}
+		}
+		
+		return getPosition(minT);
 	}
 	
 	public Transform[] getWaypoints() {
