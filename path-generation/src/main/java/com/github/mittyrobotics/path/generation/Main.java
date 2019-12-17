3b6accb25fb7caf346aa2b6bd1318146544ce7a1
Owen Leather <owenleather22@mittymonarch.com>
2019-12-16 17:47:46 -0800

    finished closest point calculations on Path

--- path-generation/src/main/java/com/github/mittyrobotics/path/generation/Main.java
+++ path-generation/src/main/java/com/github/mittyrobotics/path/generation/Main.java
@@ -1,5 +1,6 @@
 package com.github.mittyrobotics.path.generation;
 
+import com.github.mittyrobotics.datacollection.performance.TimeMonitor;
 import com.github.mittyrobotics.datatypes.positioning.Position;
 import com.github.mittyrobotics.datatypes.positioning.Transform;
 import com.github.mittyrobotics.path.generation.paths.CubicHermitePath;
@@ -15,28 +16,27 @@ public class Main {
 		
 		Graph graph = new Graph();
 		
-		graph.resizeGraph(-10,110,-10,110);
+		graph.resizeGraph(-20, 120, -20, 120);
 		
 		graph.getChart().removeLegend();
 		CubicHermitePath path = new CubicHermitePath(new Transform[]{
 				new Transform(0, 0, 0),
 				new Transform(100, 100, 0)
 		});
-		Transform transform = new Transform(0,0);
-		while(true){
-			graph.clearGraph();
-			graph.addDataset(GraphManager.getInstance().graphParametric(path.getParametrics(), 2, 1, "spline", Color.cyan));
-			Position closestPos = path.getClosestPoint(transform.getPosition(),10,false,10);
-			graph.addDataset(GraphManager.getInstance().graphArrow(new Transform(closestPos,90),5,2,"asdf",Color.green));
-			
-			transform.setPosition(transform.getPosition().add(new Position(.3,.3)));
+		Transform transform = new Transform(40, 34);
+		graph.clearGraph();
+		Position closestPos = path.getClosestPoint(transform.getPosition(), 0, false, 10,1000);
+		Position targetPos = path.getClosestPoint(closestPos, 20, false, 10,1000);
+		graph.addDataset(GraphManager.getInstance().graphParametric(path.getParametrics(), 2, 1, "spline", Color.cyan));
+		graph.addDataset(GraphManager.getInstance().graphArrow(new Transform(closestPos, 90), 5, 2, "asdf", Color.green));
+		graph.addDataset(GraphManager.getInstance().graphArrow(new Transform(targetPos, 90), 5, 2, "asdf", Color.yellow));
+		graph.addDataset(GraphManager.getInstance().graphArrow(transform, 5, 2, "asdf", Color.white));
 
-			graph.addDataset(GraphManager.getInstance().graphArrow(transform,5,2,"asdf",Color.white));
-			try {
-				Thread.sleep(30);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
+		try {
+			Thread.sleep(30);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
 		}
 	}
+	
 }
--- path-generation/src/main/java/com/github/mittyrobotics/path/generation/paths/Path.java
+++ path-generation/src/main/java/com/github/mittyrobotics/path/generation/paths/Path.java
@@ -1,7 +1,10 @@
 package com.github.mittyrobotics.path.generation.paths;
 
+import com.github.mittyrobotics.datatypes.geometry.Circle;
+import com.github.mittyrobotics.datatypes.geometry.Line;
 import com.github.mittyrobotics.datatypes.path.Parametric;
 import com.github.mittyrobotics.datatypes.positioning.Position;
+import com.github.mittyrobotics.datatypes.positioning.Rotation;
 import com.github.mittyrobotics.datatypes.positioning.Transform;
 
 public abstract class Path {
@@ -45,12 +48,62 @@ public abstract class Path {
 		return new Transform();
 	}
 	
-	public Position getClosestPoint(Position referencePosition, double distanceShift, boolean reversed, double steps) {
+	public Position getClosestPoint(Position referencePosition, double distanceShift, boolean reversed, double initialSteps, double refinedSteps) {
+		double closestDistance;
+		double distanceToStartWaypoint = referencePosition.distance(waypoints[0].getPosition());
+		double distanceToEndWaypoint = referencePosition.distance(waypoints[waypoints.length - 1].getPosition());
+		
+		Transform relativeStart = new Transform(referencePosition).relativeTo(waypoints[0]);
+		Transform relativeEnd = new Transform(referencePosition).relativeTo(waypoints[waypoints.length - 1]);
+		
+		if (reversed && relativeStart.getPosition().getX() < 0.3) {
+			Transform waypoint = waypoints[0];
+			return waypoint.getPosition().add(new Position(waypoint.getRotation().add(new Rotation(180)).cos()*distanceShift,waypoint.getRotation().add(new Rotation(180)).sin()*distanceShift));
+		} else if (!reversed && relativeEnd.getPosition().getX() > -0.3) {
+			Transform waypoint = waypoints[waypoints.length - 1];
+			return waypoint.getPosition().add(new Position(waypoint.getRotation().cos()*distanceShift,waypoint.getRotation().sin()*distanceShift));
+		}
+
+		if(distanceToStartWaypoint <= distanceShift || distanceToEndWaypoint <= distanceShift ){
+			Line line = null;
+			Transform closestWaypoint = null;
+			if(distanceToStartWaypoint <= distanceShift && reversed){
+				line = new Line(waypoints[0].getPosition(),waypoints[0].getPosition().add(new Position(waypoints[0].getRotation().cos(),waypoints[0].getRotation().sin())));
+				closestWaypoint = waypoints[0];
+			}
+			else if(distanceToEndWaypoint <= distanceShift && !reversed){
+				line = new Line(waypoints[waypoints.length-1].getPosition(),waypoints[waypoints.length-1].getPosition().add(new Position(waypoints[waypoints.length-1].getRotation().cos(),waypoints[waypoints.length-1].getRotation().sin())));
+				closestWaypoint = waypoints[waypoints.length-1];
+			}
+
+			if(line != null){
+				Circle circle = new Circle(referencePosition,distanceShift);
+				Position[] positions = circle.circleLineIntersection(line);
+				Position position = new Position();
+				for(int i = 0; i < positions.length; i++){
+					double distance = Math.abs(positions[i].distance(referencePosition)-distanceShift);
+					Position relative = new Transform(positions[i]).relativeTo(closestWaypoint).getPosition();
+					if(reversed){
+						if(relative.getX() < 0){
+							position = positions[i];
+						}
+					}
+					else{
+						if(relative.getX() > 0){
+							position = positions[i];
+						}
+					}
+				}
+				return position;
+			}
+		}
+
+		
 		double minT = 0;
 		double maxT = 0;
-		double closestDistance = 9999;
-		for (int i = 0; i < steps + 1; i++) {
-			double t = i / steps;
+		closestDistance = 9999;
+		for (int i = 0; i < initialSteps + 1; i++) {
+			double t = i / initialSteps;
 			Transform transform = getTransform(t);
 			double distance = Math.abs(transform.getPosition().distance(referencePosition));
 			if (distance < closestDistance) {
@@ -58,30 +111,30 @@ public abstract class Path {
 					if (new Transform(referencePosition).relativeTo(transform).getPosition().getX() < 0) {
 						closestDistance = distance;
 						maxT = t;
-						minT = t - 1 / steps;
-						while(referencePosition.distance(getPosition(minT)) < distanceShift){
-							minT -= 1/steps;
+						minT = t - 1 / initialSteps;
+						while (referencePosition.distance(getPosition(minT)) < distanceShift) {
+							minT -= 1 / initialSteps;
 						}
 					}
 				} else {
 					if (new Transform(referencePosition).relativeTo(transform).getPosition().getX() > 0) {
 						closestDistance = distance;
 						minT = t;
-						maxT = t + 1 / steps;
-						while(referencePosition.distance(getPosition(maxT)) < distanceShift){
-							maxT += 1/steps;
+						maxT = t + 1 / initialSteps;
+						while (referencePosition.distance(getPosition(maxT)) < distanceShift) {
+							maxT += 1 / initialSteps;
 						}
 					}
 				}
 			}
 		}
-
+		
 		double tFinal = 0;
 		
-		double secondStepRate = 0.001;
+		double secondStepRate = 1/refinedSteps;
 		
 		closestDistance = 9999;
-		if(reversed){
+		if (reversed) {
 			for (double t = minT; t < maxT; t += secondStepRate) {
 				Transform transform = getTransform(t);
 				double distance = Math.abs(transform.getPosition().distance(referencePosition) - distanceShift);
@@ -92,8 +145,7 @@ public abstract class Path {
 					}
 				}
 			}
-		}
-		else{
+		} else {
 			for (double t = minT; t < maxT; t += secondStepRate) {
 				Transform transform = getTransform(t);
 				double distance = Math.abs(transform.getPosition().distance(referencePosition) - distanceShift);
@@ -105,9 +157,8 @@ public abstract class Path {
 				}
 			}
 		}
-		//System.out.println(tFinal);
-		
-		return getPosition(tFinal);
+		Position position = getPosition(tFinal);
+		return position;
 	}
 	
 	public Transform[] getWaypoints() {
