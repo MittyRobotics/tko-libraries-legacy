aa54912ddfbe133e99fd0c52463ab203302da0a3
Owen Leather <owenleather22@mittymonarch.com>
2019-12-19 18:35:24 -0800

    increase conversion accuracy

--- datatypes/src/main/java/com/github/mittyrobotics/datatypes/units/Conversions.java
+++ datatypes/src/main/java/com/github/mittyrobotics/datatypes/units/Conversions.java
@@ -2,7 +2,7 @@ package com.github.mittyrobotics.datatypes.units;
 
 public class Conversions {
 	public static double IN_TO_M = 0.0254;
-	public static double M_TO_IN = 39.3701;
+	public static double M_TO_IN = 39.37007874;
 	public static double LBS_TO_KG = 0.453592;
 	public static double KG_TO_LBS = 2.20462;
 }
