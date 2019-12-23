b1b673daeb6ab4099010a922ca681c71176d4e91
Owen Leather <owenleather22@mittymonarch.com>
2019-12-23 11:14:35 -0800

    added module-info

---
+++ datatypes/src/main/java/module-info.java
@@ -0,0 +1,7 @@
+module tko.libraries.datatypes.main {
+	exports com.github.mittyrobotics.datatypes.geometry;
+	exports com.github.mittyrobotics.datatypes.motion;
+	exports com.github.mittyrobotics.datatypes.path;
+	exports com.github.mittyrobotics.datatypes.positioning;
+	exports com.github.mittyrobotics.datatypes.units;
+}
---
+++ simulation/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module tko.libraries.simulation.main {
+	requires java.desktop;
+	requires tko.libraries.datatypes.main;
+	requires tko.libraries.visualization.main;
+	
+	exports com.github.mittyrobotics.simulation.sim;
+	exports com.github.mittyrobotics.simulation.util;
+}
