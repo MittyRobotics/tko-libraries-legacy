f00aa4fc3bfc808be9915e7ec3a2688ca3360f9f
Owen Leather <owenleather22@mittymonarch.com>
2019-12-23 15:44:04 -0800

    added set limelight properties and enums

--- vision/src/main/java/com/github/mittyrobotics/vision/Limelight.java
+++ vision/src/main/java/com/github/mittyrobotics/vision/Limelight.java
@@ -24,6 +24,12 @@
 
 package com.github.mittyrobotics.vision;
 
+import com.github.mittyrobotics.vision.enums.LimelightCameraMode;
+import com.github.mittyrobotics.vision.enums.LimelightLEDMode;
+import com.github.mittyrobotics.vision.enums.LimelightSnapshotMode;
+import com.github.mittyrobotics.vision.enums.LimelightStreamMode;
+import edu.wpi.first.networktables.NetworkTableInstance;
+
 /**
  * Singleton that handles all Limelight related tasks and functions.
  */
@@ -34,5 +40,107 @@ public class Limelight {
 		return ourInstance;
 	}
 	
+	private int pipelineID = 0;
+	
+	private Limelight() {
+	
+	}
+	
+	public void initDefaultLimelightSettings(){
+		setPipeline(0);
+		setLedMode(LimelightLEDMode.On);
+		setCameraMode(LimelightCameraMode.Vision);
+		setStreamMode(LimelightStreamMode.Secondary);
+		setSnapshotMode(LimelightSnapshotMode.Off);
+	}
+	
+	/**
+	 * Sets the LED mode on the Limelight camera.
+	 * <p>
+	 * 0	use the LED Mode set in the current pipeline
+	 * 1	force off
+	 * 2	force blink
+	 * 3	force on
+	 *
+	 * @param ledMode enum containing the different modes for leds.
+	 */
+	public void setLedMode(LimelightLEDMode ledMode) {
+		NetworkTableInstance.getDefault().getTable("limelight").getEntry("ledMode").setNumber(ledMode.value); //	Sets limelight’s LED state
+	}
+	
+	/**
+	 * Sets the camera mode on the Limelight camera.
+	 * <p>
+	 * 0	Vision processor
+	 * 1	Driver Camera (Increases exposure, disables vision processing) pipeline	Sets limelight’s current pipeline
+	 *
+	 * @param cameraMode enum containing the different modes of camera
+	 */
+	public void setCameraMode(LimelightCameraMode cameraMode) {
+		NetworkTableInstance.getDefault().getTable("limelight").getEntry("camMode").setNumber(cameraMode.value); //	Sets limelight’s operation mode
+	}
+	
+	/**
+	 * Sets the active pipeline on the Limelight camera. These pipelines are
+	 * configured in the limelight configuration tool and range from 0-9.
+	 *
+	 * @param pipelineID ID of new active pipeline
+	 */
+	public void setPipeline(int pipelineID) {
+		this.pipelineID = pipelineID;
+		if (pipelineID > 9 || pipelineID < 0) {
+			pipelineID = 0;
+		}
+		NetworkTableInstance.getDefault().getTable("limelight").getEntry("pipeline").setNumber(pipelineID); //	Sets limelight’s operation mode
+	}
+	
+	/**
+	 * Sets the stream configuration on the Limelight camera.
+	 * <p>
+	 * 0	Standard - Side-by-side streams if a webcam is attached to Limelight
+	 * 1	PiP Main - The secondary camera stream is placed in the lower-right corner of the primary camera stream
+	 * 2	PiP Secondary - The primary camera stream is placed in the lower-right corner of the secondary camera stream
+	 *
+	 * @param streamMode enum containing the different streaming configurations
+	 */
+	public void setStreamMode(LimelightStreamMode streamMode) {
+		NetworkTableInstance.getDefault().getTable("limelight").getEntry("stream").setNumber(streamMode.value); //Sets limelight’s streaming mode
+	}
+	
+	/**
+	 * Sets the snapshot mode on the Limelight camera.
+	 * <p>
+	 * 0	Stop taking snapshots
+	 * 1	Take two snapshots per second
+	 *
+	 * @param snapshotMode enum containing the different snapshot modes
+	 */
+	public void setSnapshotMode(LimelightSnapshotMode snapshotMode) {
+		NetworkTableInstance.getDefault().getTable("limelight").getEntry("snapshot").setNumber(snapshotMode.value); //Allows users to take snapshots during a match
+	}
+	
+	/**
+	 * Puts the Limelight into vision mode, with the camera settings turned to vision mode (low exposure)
+	 * and the LEDs on.
+	 * <p>
+	 * Vision mode sets the camera properties to allow for optimum vision tracking and turns the LEDs on so the
+	 * target can be tracked.
+	 */
+	public void enableVisionMode() {
+		setCameraMode(LimelightCameraMode.Vision);
+		setLedMode(LimelightLEDMode.On);
+	}
+	
+	/**
+	 * Puts the Limelight into driver mode, with the camera settings turned to driver mode (regular exposure)
+	 * and the LEDs off.
+	 * <p>
+	 * Driver mode allows the driver to see through the Limelight camera and turns off the LEDs so it does not
+	 * distract others on the field.
+	 */
+	public void enableDriverMode() {
+		setCameraMode(LimelightCameraMode.Driver);
+		setLedMode(LimelightLEDMode.Off);
+	}
 	
 }
