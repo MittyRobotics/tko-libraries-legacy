880979ba959213268b1a8eba411041827f37eea8
Owen Leather <owenleather22@mittymonarch.com>
2019-12-26 17:10:03 -0800

    improved conversion values

--- datatypes/src/main/java/com/github/mittyrobotics/datatypes/Main.java
+++ datatypes/src/main/java/com/github/mittyrobotics/datatypes/Main.java
@@ -26,10 +26,12 @@ package com.github.mittyrobotics.datatypes;
 
 import com.github.mittyrobotics.datatypes.geometry.Circle;
 import com.github.mittyrobotics.datatypes.positioning.Position;
+import com.github.mittyrobotics.datatypes.units.Conversions;
 
 public class Main {
     public static void main(String[] args) {
         Position point = new Circle(new Position(5, 5), 6).getClosestPoint(new Position(15, 13));
         System.out.println(point);
+        System.out.println(Conversions.IN_TO_M);
     }
 }
