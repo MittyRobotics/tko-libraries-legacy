538b9c5e5f23e97c62175673b040b2a5fbdd9edb
Ashvin Iyer <ashviniyer21@mittymonarch.com>
2020-04-02 16:31:41 -0700

    Adding continuous support and output range to PIDF controller

---
+++ motion/src/main/java/com/github/mittyrobotics/motion/controllers/ContinousManager.java
@@ -0,0 +1,57 @@
+package com.github.mittyrobotics.motion.controllers;
+
+public class ContinousManager {
+    private double minInput, maxInput;
+    private double inputRange;
+
+    public ContinousManager(double minInput, double maxInput){
+        setInputRange(minInput, maxInput);
+    }
+
+    public void setInputRange(double minInput, double maxInput){
+        this.minInput = minInput;
+        this.maxInput = maxInput;
+        inputRange = maxInput - minInput;
+    }
+
+    public double getMinInput() {
+        return minInput;
+    }
+
+    public double getMaxInput() {
+        return maxInput;
+    }
+
+    public boolean isEnabled() {
+        return inputRange > 0;
+    }
+
+    public double getInputRange(){
+        return inputRange;
+    }
+
+    public double getContinousError(double setpoint, double measurement){
+        if(isEnabled() && measurement >= minInput && measurement <= maxInput){
+            double error = (setpoint - measurement) % inputRange;
+            if (Math.abs(error) > inputRange / 2) {
+                if (error > 0) {
+                    return error - inputRange;
+                } else {
+                    return error + inputRange;
+                }
+            }
+        }
+        return setpoint - measurement;
+    }
+
+    public double mapValue(double value){
+        if(isEnabled()){
+            value %= inputRange;
+            if(value < 0){
+                value += inputRange;
+            }
+            value += minInput;
+        }
+        return value;
+    }
+}
